7d435165ecf2b604c541f271f8b6f36dcb18d814
Ali Farooq <blueprintmattmorgan@gmail.com>
2018-02-16 19:30:07 +0500

    Add files via upload
    fixed issues pointed out on Fiverr chat

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
 (function ($) {
 
-// var jump=function(e)
-// {
-//    if (e){
-//        e.preventDefault();
-//        var target = $(this).attr("href");
-//    }else{
-//        var target = location.hash;
-//    }
-//
-//    $('html,body').animate(
-//    {
-//        scrollTop: $(target).offset().top
-//    },1000,function()
-//    {
-//        location.hash = target;
-//    });
-//
-//    // window.history.replaceState({}, document.title, "index.html");
-//
-// }
-
-// $('html, body').hide();
-
     $(document).ready(function () {
 
         //////////jQuery Fullpage plugin Settings//////////
@@ -48,14 +25,38 @@
 
         //////////Animations Code//////////
 
+        //control navigation colors
+        var menuTl = new TimelineMax({paused:true});
 
-        //Line Animation Section 1
+        var controller = new ScrollMagic.Controller();
+
+        var startScroll = new ScrollMagic.Scene({
+            triggerElement: "#scrollto-section-7",
+            triggerHook: "onEnter",
+            reverse:true
+        });
+
+        startScroll.on("enter", function(){
+            $('#top-menu').removeClass('inverted');
+            $('#top-logo').attr('src', "img/bottom_logo.svg");
+            $('#white-top').delay(20).fadeIn(90);
+        });
+
+        startScroll.on("leave", function(){
+            $('#top-menu').addClass('inverted');
+            $('#top-logo').attr('src', "img/top_logo.svg");
+            $('#white-top').fadeOut(90);
+        });
+
+        startScroll.addTo(controller);
+
+        ///////////////////////Line Animation Section 1
         var blueFill = document.getElementById("blue-fill"),
             redFill = document.getElementById("red-fill"),
             greenFill = document.getElementById("green-fill"),
             orangeFill = document.getElementById("orange-fill");
 
-        var anim1tl = new TimelineMax({repeat:-1});
+        var anim1tl = new TimelineMax({repeat:-1,paused:true});
         anim1tl.set([blueFill,redFill,greenFill,orangeFill],{attr:{x1:17428.4883,x2:19918.5117}});
         anim1tl.to(orangeFill,1.5,{attr:{x1:22428.4883}},0)
             .to(orangeFill,1.5,{attr:{x2:24918.5117}},0)
@@ -82,7 +83,23 @@
             green2 = document.querySelector("#XMLID_66_"),
             text = document.querySelector("#text");
 
-        //Graph Animation Section 2
+        var startStopAnim1 = new ScrollMagic.Scene({
+            triggerElement: "#scrollto-section-1",
+            duration:"100%"
+        });
+
+        startStopAnim1.on("enter", function(){
+            anim1tl.play();
+        });
+
+        startStopAnim1.on("leave", function(){
+            anim1tl.restart();
+            anim1tl.pause(0);
+        });
+
+        startStopAnim1.addTo(controller);
+
+        ///////////////////////Graph Animation Section 2
         var anim2tl = new TimelineMax({repeat:-1});
         anim2tl.to(graph1,24,{x:-1500,ease:Linear.easeNone});
 
@@ -118,10 +135,11 @@
             .to(green2,0.25,{opacity:0})
             .to(green2,0.25,{drawSVG:"0"});
 
-        var anim2tl5 = new TimelineMax({repeat:-1,delay:6.6,repeatDelay:9});
+        var anim2tl5 = new TimelineMax({repeat:-1,delay:9.8,repeatDelay:15});
         anim2tl5.set(text,{opacity:0})
             .to(text,0.15,{opacity:1})
             .to(text,2.85,{opacity:0,y:-65,onComplete:changeValue});
+        anim2tl5.timeScale(1.5);
 
         function changeValue () {
             var val = Math.floor(Math.random()*(500-50+1)+50);
@@ -132,22 +150,48 @@
         //Coin Animation Section 4
         var group1 = document.querySelector("#group1"),
             group2 = document.querySelector("#group2"),
-            group3 = document.querySelector("#group3");
+            group3 = document.querySelector("#group3"),
+            coinSVG = document.querySelector("#coin-svg");
 
-        var anim3tl = new TimelineMax({repeat:-1});
-        anim3tl.to(group1,2,{y:-10,ease:Power1.easeInOut})
-            .to(group1,2,{y:0,ease:Power1.easeInOut});
-        anim3tl.timeScale(0.3);
+        var anim3main = new TimelineMax({paused:true,onComplete:coinHover});
+        anim3main.set(coinSVG,{transformOrigin:"center center",scale:0})
+            .to(coinSVG,0.1,{opacity:1})
+            .to(coinSVG,2,{scale:1});
 
-        var anim3tl2 = new TimelineMax({repeat:-1,delay:1});
-        anim3tl2.to(group2,2,{y:10,ease:Power1.easeInOut})
-            .to(group2,2,{y:0,ease:Power1.easeInOut});
-        anim3tl2.timeScale(0.3);
+        function coinHover () {
 
-        var anim3tl3 = new TimelineMax({repeat:-1,delay:1});
-        anim3tl3.to(group3,2,{y:-10,ease:Power1.easeInOut})
-            .to(group3,2,{y:0,ease:Power1.easeInOut});
-        anim3tl3.timeScale(0.3);
+            var anim3tl = new TimelineMax({repeat:-1});
+            anim3tl.to(group1,2,{y:-10,ease:Power1.easeInOut})
+                .to(group1,2,{y:0,ease:Power1.easeInOut});
+            anim3tl.timeScale(0.3);
+
+            var anim3tl2 = new TimelineMax({repeat:-1,delay:1});
+            anim3tl2.to(group2,2,{y:10,ease:Power1.easeInOut})
+                .to(group2,2,{y:0,ease:Power1.easeInOut});
+            anim3tl2.timeScale(0.3);
+
+            var anim3tl3 = new TimelineMax({repeat:-1,delay:1});
+            anim3tl3.to(group3,2,{y:-10,ease:Power1.easeInOut})
+                .to(group3,2,{y:0,ease:Power1.easeInOut});
+            anim3tl3.timeScale(0.3);
+
+        }
+
+        var startStopAnim4 = new ScrollMagic.Scene({
+            triggerElement: "#scrollto-section-4",
+            duration:"100%"
+        });
+
+        startStopAnim4.addTo(controller);
+
+        startStopAnim4.on("enter", function(event){
+            anim3main.restart();
+            anim3main.play();
+        });
+
+        startStopAnim4.on("leave", function(event){
+            anim3main.pause(0);
+        });
 
         //Castle Animation Section 6
         var cloud1 = document.querySelector("#cloud-1"),
@@ -169,25 +213,26 @@
             .to(flag1,1.5,{morphSVG:flag2,ease:Linear.easeNone})
             .to(flag1,1.5,{morphSVG:flag1,ease:Linear.easeNone});
 
+        var startStopAnim6 = new ScrollMagic.Scene({
+            triggerElement: "#scrollto-section-6",
+            duration:"100%"
+        });
+
+        startStopAnim6.addTo(controller);
+
+        startStopAnim6.on("enter", function(event){
+            anim4tl.restart();
+            anim4tl2.restart();
+            anim4tl.play();
+            anim4tl2.play();
+        });
+
+        startStopAnim6.on("leave", function(event){
+            anim4tl.pause(0);
+            anim4tl2.pause(0);
+        });
+
         //////////Animations Code End//////////
-
-        //
-
-        // $.scrollify({
-        //     section : ".scrollify-section"
-        // });
-
-        // $('a[href^=#]').bind("click", jump);
-
-        // if (location.hash){
-        //     setTimeout(function(){
-        //         $('html, body').scrollTop(0).show();
-        //         jump()
-        //
-        //     }, 0);
-        // }else{
-        //   $('html, body').show()
-        // }
 
         $(window).resize(function () {
 
@@ -203,9 +248,6 @@
             $('#input-early-access').attr('placeholder', 'Enter your email');
             $('#bottom-input-early-access').attr('placeholder', 'Enter your email');
         }
-
-        // $('body').sectionScroll();
-
 
         $('body').on('section-reached', function () {
 
@@ -245,28 +287,6 @@
                 }
             }
         });
-
-        $('#footer').appear();
-
-        $('#footer').on('appear', function (event, $all_appeared_elements) {
-            $('#top-menu').removeClass('inverted');
-            $('#top-logo').attr('src', "img/bottom_logo.svg");
-            $('#white-top').delay(20).fadeIn(90);
-        });
-
-        $('#footer').on('disappear', function (event, $all_disappeared_elements) {
-            $('#top-menu').addClass('inverted');
-            $('#top-logo').attr('src', "img/top_logo.svg");
-            $('#white-top').fadeOut(90);
-        });
-
-        // $('#home-btn').click(function(){
-        //   $('body').scrollTo('#scrollto-section-1', 500);
-        // });
-        //
-        // $('#features-btn').click(function(){
-        //   $('body').scrollTo('#scrollto-section-2', 500);
-        // });
 
         $('.close_pop1').click(function () {
             $('#sign-up-ovrly').fadeOut(300, "linear");
