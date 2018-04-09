a1a1366c58256d21927a0dcadf88e67c5e86e59d
AliFarooq0072 <asadwaleed93@gmail.com>
2018-04-09 12:32:25 +0500

    FAQ & Privacy/Terms - paragraph spacing

--- src/components/views/Support.js
+++ src/components/views/Support.js
@@ -12,39 +12,39 @@ class Support extends Component {
             faqs: [
                 {
                     question: 'What is Cointec?',
-                    answer: 'Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.<br><br>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.<br><br>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.'
+                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>'
                 },
                 {
                     question: 'Who can order?',
-                    answer: 'Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.<br><br>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.<br><br>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.'
+                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>'
                 },
                 {
                     question: 'What payment methods do you accept?',
-                    answer: 'In order to make sure brokers received funds and release coins immediately we only allow users to send Bank-transfers with faster-payments enabled bank accounts.<br><br>Faster Payments is the UK’s 24/7 Real Time Payment System that allows individuals and businesses to make near instant payments.<br><br>Today the majority of UK bank accounts that offer online-banking make use of Faster Payments.<br><br>You can check if your bank account is enabled on the official website.'
+                    answer: '<p>In order to make sure brokers received funds and release coins immediately we only allow users to send Bank-transfers with faster-payments enabled bank accounts.</p><p>Faster Payments is the UK’s 24/7 Real Time Payment System that allows individuals and businesses to make near instant payments.</p><p>Today the majority of UK bank accounts that offer online-banking make use of Faster Payments.</p><p>You can check if your bank account is enabled on the official website.</p>'
                 },
                 {
                     question: 'Do you store any Bitcoins on your platform?',
-                    answer: 'No, for security reasons, we don\'t store any currencies of any form at all.<br><br>We are a Peer-To-Peer platform that connects you to a professional digital asset vendor. Once you order with cryptomonster, the trade is between two professional individuals on our platform. Our job is to ensure that the platform runs smoothly, all vendors are monitored & the growth of our platform continues.<br><br>Using Peer-To-Peer methodology, your funds aren\'t \'stored\' anywhere during transit and therefore cannot be stolen. Funds are proccessed once an order is fully paid for to the individual Vendor, not before, and subsequently sent to the destination wallet you provide immediately there after.'
+                    answer: '<p>No, for security reasons, we don\'t store any currencies of any form at all.</p><p>We are a Peer-To-Peer platform that connects you to a professional digital asset vendor. Once you order with cryptomonster, the trade is between two professional individuals on our platform. Our job is to ensure that the platform runs smoothly, all vendors are monitored & the growth of our platform continues.</p><p>Using Peer-To-Peer methodology, your funds aren\'t \'stored\' anywhere during transit and therefore cannot be stolen. Funds are proccessed once an order is fully paid for to the individual Vendor, not before, and subsequently sent to the destination wallet you provide immediately there after.</p>'
                 },
                 {
                     question: 'How do I increase my order limit?',
-                    answer: 'Cointec is currently in Beta testing where the maximum daily limit for all verified users is £250. Once the stable version is released, your order limit will be determined by xyz.'
+                    answer: '<p>Cointec is currently in Beta testing where the maximum daily limit for all verified users is £250. Once the stable version is released, your order limit will be determined by xyz.</p>'
                 },
                 {
                     question: 'Help! My order has been cancelled',
-                    answer: 'Your order was cancelled because we did not receive your payment within the 5 minute window after you pressed the ‘I have made payment’ button. Any funds received after the 5 minute window will be returned to the same account within 1-2 business days. If you believe your order was cancelled for another reason, please contact support.'
+                    answer: '<p>Your order was cancelled because we did not receive your payment within the 5 minute window after you pressed the ‘I have made payment’ button. Any funds received after the 5 minute window will be returned to the same account within 1-2 business days. If you believe your order was cancelled for another reason, please contact support.</p>'
                 },
                 {
                     question: 'How do I create a Bitcoin wallet',
-                    answer: 'In order to buy Bitcoins on our platform you must already have an external Bitcoin wallet. We recommend using hardware wallets such as Ledger Nano S and TREZOR as these provide the most security.'
+                    answer: '<p>In order to buy Bitcoins on our platform you must already have an external Bitcoin wallet. We recommend using hardware wallets such as Ledger Nano S and TREZOR as these provide the most security.</p>'
                 },
                 {
                     question: 'What cryptocurrencies can I buy on Cointec?',
-                    answer: 'Cointec is currently in Beta testing where users can only buy Bitcoins. However, we plan to offer multiple cryptocurrencies as part of the stable release.'
+                    answer: '<p>Cointec is currently in Beta testing where users can only buy Bitcoins. However, we plan to offer multiple cryptocurrencies as part of the stable release.</p>'
                 },
                 {
                     question: 'How do I buy Bitcoins?',
-                    answer: `Buying Bitcoins on Cointec is easy. Follow the steps below to get started.<br><br>
+                    answer: `<p>Buying Bitcoins on Cointec is easy. Follow the steps below to get started.</p>
                         1. Create an accout<br>
                         2. Get verified by telling us a little about yourself<br>
                         3. Use our calculator on the home page to start buying
@@ -85,11 +85,11 @@ class Support extends Component {
                 },
                 {
                     question: 'What fees can I expect to pay?',
-                    answer: 'You will only ever deal with a competitive exchange rate, with no fees involved. We take our commission from the brokers who sell on our platform.'
+                    answer: '<p>You will only ever deal with a competitive exchange rate, with no fees involved. We take our commission from the brokers who sell on our platform.</p>'
                 },
                 {
                     question: 'How long can will it take to receive my Bitcoins?',
-                    answer: 'Once we have recieved your payment, your Bitcoins will be sent instantly to your external wallet. The time it takes for the Bitcoins to show up in your wallet will depend on how the fast the transaction gets confirmed. You can check your transaction\'s confirmation status and other payment details on any blockchain explorer.'
+                    answer: '<p>Once we have recieved your payment, your Bitcoins will be sent instantly to your external wallet. The time it takes for the Bitcoins to show up in your wallet will depend on how the fast the transaction gets confirmed. You can check your transaction\'s confirmation status and other payment details on any blockchain explorer.</p>'
                 },
             ]
         }
