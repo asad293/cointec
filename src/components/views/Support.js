dde6ba19fd40df62ff67fdde93e2287c914b90db
AliFarooq0072 <asadwaleed93@gmail.com>
2018-04-13 22:30:45 +0500

    Faq search working

--- src/components/views/Support.js
+++ src/components/views/Support.js
@@ -9,38 +9,48 @@ class Support extends Component {
     constructor() {
         super()
         this.state = {
+            search: '',
             faqs: [
                 {
                     question: 'What is Cointec?',
-                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>'
+                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>',
+                    keywords: []
                 },
                 {
                     question: 'Who can order?',
-                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>'
+                    answer: '<p>Cointec is the trading name of Cointec LTD, a UK start up specialized in making digital currency accessible to everyone.</p><p>We connect buyers to only the most trusted and responsive brokers around the country to deliver a fast and reliable experience.</p><p>Our platform is built by early cryptocurrency enthusiasts with experience also in the technology and payments industries.</p>',
+                    keywords: []
                 },
                 {
                     question: 'What payment methods do you accept?',
-                    answer: '<p>In order to make sure brokers received funds and release coins immediately we only allow users to send Bank-transfers with faster-payments enabled bank accounts.</p><p>Faster Payments is the UK’s 24/7 Real Time Payment System that allows individuals and businesses to make near instant payments.</p><p>Today the majority of UK bank accounts that offer online-banking make use of Faster Payments.</p><p>You can check if your bank account is enabled on the official website.</p>'
+                    answer: '<p>In order to make sure brokers received funds and release coins immediately we only allow users to send Bank-transfers with faster-payments enabled bank accounts.</p><p>Faster Payments is the UK’s 24/7 Real Time Payment System that allows individuals and businesses to make near instant payments.</p><p>Today the majority of UK bank accounts that offer online-banking make use of Faster Payments.</p><p>You can check if your bank account is enabled on the official website.</p>',
+                    keywords: []
                 },
                 {
                     question: 'Do you store any Bitcoins on your platform?',
-                    answer: '<p>No, for security reasons, we don\'t store any currencies of any form at all.</p><p>We are a Peer-To-Peer platform that connects you to a professional digital asset vendor. Once you order with cryptomonster, the trade is between two professional individuals on our platform. Our job is to ensure that the platform runs smoothly, all vendors are monitored & the growth of our platform continues.</p><p>Using Peer-To-Peer methodology, your funds aren\'t \'stored\' anywhere during transit and therefore cannot be stolen. Funds are proccessed once an order is fully paid for to the individual Vendor, not before, and subsequently sent to the destination wallet you provide immediately there after.</p>'
+                    answer: '<p>No, for security reasons, we don\'t store any currencies of any form at all.</p><p>We are a Peer-To-Peer platform that connects you to a professional digital asset vendor. Once you order with cryptomonster, the trade is between two professional individuals on our platform. Our job is to ensure that the platform runs smoothly, all vendors are monitored & the growth of our platform continues.</p><p>Using Peer-To-Peer methodology, your funds aren\'t \'stored\' anywhere during transit and therefore cannot be stolen. Funds are proccessed once an order is fully paid for to the individual Vendor, not before, and subsequently sent to the destination wallet you provide immediately there after.</p>',
+                    keywords: []
                 },
                 {
                     question: 'How do I increase my order limit?',
-                    answer: '<p>Cointec is currently in Beta testing where the maximum daily limit for all verified users is £250. Once the stable version is released, your order limit will be determined by xyz.</p>'
+                    answer: '<p>Cointec is currently in Beta testing where the maximum daily limit for all verified users is £250. Once the stable version is released, your order limit will be determined by xyz.</p>',
+                    keywords: []
                 },
                 {
                     question: 'Help! My order has been cancelled',
-                    answer: '<p>Your order was cancelled because we did not receive your payment within the 5 minute window after you pressed the ‘I have made payment’ button. Any funds received after the 5 minute window will be returned to the same account within 1-2 business days. If you believe your order was cancelled for another reason, please contact support.</p>'
+                    answer: '<p>Your order was cancelled because we did not receive your payment within the 5 minute window after you pressed the ‘I have made payment’ button. Any funds received after the 5 minute window will be returned to the same account within 1-2 business days. If you believe your order was cancelled for another reason, please contact support.</p>',
+                    keywords: []
                 },
                 {
                     question: 'How do I create a Bitcoin wallet',
-                    answer: '<p>In order to buy Bitcoins on our platform you must already have an external Bitcoin wallet. We recommend using hardware wallets such as Ledger Nano S and TREZOR as these provide the most security.</p>'
+                    answer: '<p>In order to buy Bitcoins on our platform you must already have an external Bitcoin wallet. We recommend using hardware wallets such as Ledger Nano S and TREZOR as these provide the most security.</p>',
+                    keywords: []
                 },
                 {
                     question: 'What cryptocurrencies can I buy on Cointec?',
                     answer: '<p>Cointec is currently in Beta testing where users can only buy Bitcoins. However, we plan to offer multiple cryptocurrencies as part of the stable release.</p>'
+                    ,
+                    keywords: []
                 },
                 {
                     question: 'How do I buy Bitcoins?',
@@ -48,7 +58,8 @@ class Support extends Component {
                         1. Create an accout<br>
                         2. Get verified by telling us a little about yourself<br>
                         3. Use our calculator on the home page to start buying
-                    `
+                    `,
+                    keywords: []
                 },
                 {
                     question: 'What forms of ID are acceptable?',
@@ -58,7 +69,8 @@ class Support extends Component {
                         <li>Provisional driving license (photocard)</li>
                     </ul>
                     <p>IDs must be in date at the time of upload. For passports please upload the entire photo page, for all other forms of ID a scan of just the front is acceptable.</p>
-                    `
+                    `,
+                    keywords: []
                 },
                 {
                     question: 'What documents are accepted as proof of address?',
@@ -73,7 +85,8 @@ class Support extends Component {
                         <li>Bank statement</li>
                     </ul>
                     <p>All documetns must be dated with the last 3 months. If docments are digital formats (e.g. PDF bank statements) they must be the original downloaded file.</p>
-                    `
+                    `,
+                    keywords: []
                 },
                 {
                     question: 'How do I take an ID selfie?',
@@ -81,18 +94,40 @@ class Support extends Component {
                         <li>Face the camera</li>
                         <li>Hold a valid ID and ensure it is visible</li>
                         <li>Hold a note with the following in writing, “Cointec” followed by your name and current date. Ensure the note is visible</li>
-                    </ul>`
+                    </ul>`,
+                    keywords: []
                 },
                 {
                     question: 'What fees can I expect to pay?',
-                    answer: '<p>You will only ever deal with a competitive exchange rate, with no fees involved. We take our commission from the brokers who sell on our platform.</p>'
+                    answer: '<p>You will only ever deal with a competitive exchange rate, with no fees involved. We take our commission from the brokers who sell on our platform.</p>',
+                    keywords: []
                 },
                 {
                     question: 'How long can will it take to receive my Bitcoins?',
-                    answer: '<p>Once we have recieved your payment, your Bitcoins will be sent instantly to your external wallet. The time it takes for the Bitcoins to show up in your wallet will depend on how the fast the transaction gets confirmed. You can check your transaction\'s confirmation status and other payment details on any blockchain explorer.</p>'
+                    answer: '<p>Once we have recieved your payment, your Bitcoins will be sent instantly to your external wallet. The time it takes for the Bitcoins to show up in your wallet will depend on how the fast the transaction gets confirmed. You can check your transaction\'s confirmation status and other payment details on any blockchain explorer.</p>',
+                    keywords: []
                 },
             ]
         }
+    }
+
+    searchFaqs(event) {
+        this.setState({ search: event.target.value })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+    }
+
+    filterFaqs(faq) {
+        let words = this.state.search.toLowerCase().split(' ')
+        for (let index = 0; index < words.length; index++) {
+            const word = words[index];
+            if (!faq.question.toLowerCase().includes(word) && !faq.answer.toLowerCase().includes(word)) {
+                return false;
+            }
+        }
+        return true;
     }
     
     render() {
@@ -113,9 +148,9 @@ class Support extends Component {
 
                             <div className="row">
                                 <div className="col-md-12">
-                                    <form className="row d-flex justify-content-center">
+                                    <form className="row d-flex justify-content-center" onSubmit={this.handleSubmit.bind(this)}>
                                         <div className="search-bar mb-2 d-md-block col-lg-8 col-md-12">
-                                            <input type="text" className="py-2" placeholder="Type your question here" />
+                                            <input type="text" className="py-2" value={this.state.search} onChange={this.searchFaqs.bind(this)} placeholder="Type your question here" />
                                             <img src="./img/search.svg" alt="search" />
                                         </div>
                                     </form>
@@ -133,10 +168,15 @@ class Support extends Component {
                             <div className="col-lg-8 col-md-12">
                                 <Accordion>
                                     {
-                                        this.state.faqs.map((faq, index) => <Panel id={index} 
+                                        this.state.search ? this.state.faqs.filter(faq => this.filterFaqs(faq)).map((faq, index) => <Panel id={index} 
                                             key={index} 
                                             heading={faq.question} 
-                                            body={faq.answer} />)
+                                            body={faq.answer} />
+                                        ) : this.state.faqs.map((faq, index) => <Panel id={index} 
+                                            key={index} 
+                                            heading={faq.question} 
+                                            body={faq.answer} />
+                                        )
                                     }
                                 </Accordion>
                             </div>
