f6c1c85aacd0c965ccb1eb0253833d67aa381dd4
AliFarooq0072 <blueprintmattmorgan@gmail.com>
2018-12-24 19:03:54 +0500

    login error msg & close account error

--- components/dashboard/NotificationAlert.js
+++ components/dashboard/NotificationAlert.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import cn from 'classnames'
 
-const NotificationAlert = ({ children, onHide }) => (
-	<div className="alert-message alert-success">
-		<div className="d-flex justify-content-center align-items-center container position-relative">
-			{children}
-			<a className="btn-hide-alert" onClick={onHide}>
-				<i className="far fa-times fa-lg" />
-			</a>
+const types = {
+	success: 'alert-success',
+	danger: 'alert-danger'
+}
+
+const NotificationAlert = ({ children, onHide, type, visible }) =>
+	visible ? (
+		<div className={cn('alert-message', types[type])}>
+			<div className="d-flex justify-content-center align-items-center container position-relative">
+				{children}
+				<a className="btn-hide-alert" onClick={onHide}>
+					<i className="far fa-times fa-lg" />
+				</a>
+			</div>
 		</div>
-	</div>
-)
+	) : null
 
 export default NotificationAlert
