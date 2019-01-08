f32fef349cfc179c58846fd4b56453d88a3ccc78
AliFarooq0072 <blueprintmattmorgan@gmail.com>
2019-01-08 17:55:25 +0500

    bugfix

--- store/actions/accounts.js
+++ store/actions/accounts.js
@@ -507,7 +507,7 @@ export const validateToken = ({ action, token }) => async dispatch => {
 	dispatch({ type: VALIDATE_TOKEN_START })
 
 	return axios
-		.get(`${ROOT_URL}/accounts/${action}?token=${token}`)
+		.get(`${ROOT_URL}/validate?action=${action}&token=${token}`)
 		.then(response => {
 			dispatch({
 				type: VALIDATE_TOKEN,
