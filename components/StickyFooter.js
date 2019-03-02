85a15abafdb38518be25829c8f1ba43a5afa2b84
AliFarooq0072 <blueprintmattmorgan@gmail.com>
2019-03-02 14:41:32 +0500

    footer bug

--- components/StickyFooter.js
+++ components/StickyFooter.js
@@ -5,7 +5,7 @@ import cn from 'classnames'
 const StickyFooter = props => (
 	<div
 		className={cn('bottom-bar', props.className)}
-		style={{ position: props.fixed ? 'fixed' : 'static' }}>
+		style={{ position: props.fixed ? 'fixed' : '' }}>
 		<div className="container">
 			<div className="row">
 				<div className="col-6 copyright">@ Cointec LTD 2018</div>
